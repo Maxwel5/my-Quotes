96ba49e4b2b7278fd6d4bb2a871c696c48032eed
Maxwel5 <maxwellwafulasim@gmail.com>
2019-08-19 08:14:08 +0300

    Writing my first quote

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Quotes';
+  quote = 'The pessimist sees difficulty in every opportunity';
 }
