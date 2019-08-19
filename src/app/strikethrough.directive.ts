d1502c7170b2a62457748be1e3b4d96d1550d23b
Maxwel5 <maxwellwafulasim@gmail.com>
2019-08-19 11:47:12 +0300

    Activating the directive functionality

--- src/app/strikethrough.directive.ts
+++ src/app/strikethrough.directive.ts
@@ -6,6 +6,8 @@ import { Directive,ElementRef } from '@angular/core';
 export class StrikethroughDirective {
 
   constructor(private elem:ElementRef) {
+  
+  p  
     this.elem.nativeElement.style.textDecoration='line-through';
    }
 
