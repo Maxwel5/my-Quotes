20a3152feafee0604d11f235b6339983c8767369
Maxwel5 <maxwellwafulasim@gmail.com>
2019-08-19 10:04:14 +0300

    Providing toggle show event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
-
+import { Quote } from './quote';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  quotes:string[];
+  quotes:quote = [
+    {quote:'', author:''}
+    {quote:'', author:''}
+    {quote:'', author:''}
+    {quote:'', author:''}
+    {quote:'', author:''}
+  ];
 
   constructor(){
     this.quotes = ['The pessimist sees difficulty in every opportunity', 'When you wake up each morning, you can choose to be happy or choose to be sad', 'You learn more from failure than from success']
--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,4 +1,6 @@
 export class Quote {
-    quote: string;
-    author:string;
+    public showDescription: boolean;
+    constructor(public quote: string, public author: string, public submitter: string, public completeDate: Date){
+        this.showDescription=false;
+    }
 }
